05c1d023d765afb9f1fc3fdf1e645f8de2025c1b
Alexander88226 <abhfigaro201908@gmail.com>
2020-08-19 02:46:13 +0300

    fix regular expression

--- sead_part1_task2.py
+++ sead_part1_task2.py
@@ -4,9 +4,10 @@ def check_email(regex, email):
     if(re.search(regex,email)):  
         print("Valid Email")
     else:  
-        print("Invalid Email")  
-regex = '^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$'
-# email addres has to start with character within a-z, A-Z, 0-9 and may include ., english character, digit
-# and should have one @ symbol, and include '.' punctation, end with domain name(english character, digit)
+        print("Invalid Email")
+
+regex = "^[a-zA-Z0-9!#$%&'*+/=?^_‘{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_‘{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?)+((.com)|(.co.uk)|(.net)|(.ac.uk))$"
+# email addres has to start with character within a-z, A-Z, 0-9, printable punctations an include a-z, A-Z, 0-9, printable punctations
+# and should have one @ symbol, and last doman part should be .com, .co.uk, .net, .ac.uk
 check_email(regex, 'ankitrai326@gmail.com')
-check_email(regex, 'AnkItrai326@gmail.com')
+check_email(regex, 'An.kItr._ai326@gmail.com')
