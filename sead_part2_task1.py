b74ab4b9480e8229c12df541d2f8bd6e03a123ac
Alexander88226 <abhfigaro201908@gmail.com>
2020-08-18 19:15:10 +0300

    nltk wordnet brown corpus

---
+++ sead_part2_task1.py
@@ -0,0 +1,85 @@
+# Part-2: (Python program)
+
+# Displays all the senses and their definitions corresponding to the keyword as a noun as they appear in WordNet. 
+# If the keyword does not appear in WordNet as noun, the program displays a message and does not go to the next step.
+
+import nltk
+from nltk.corpus import wordnet as wn
+
+# get keyword
+keyword = input("Enter a word: ")
+# get Noun synsets from wordnet 
+keyword_synsets = wn.synsets(keyword, pos=wn.NOUN)
+
+# check if keyword does not appear in WordNet as noun
+if not keyword_synsets:
+    print("keyword :", keyword, " is not a Noun")
+    exit(0)
+
+# display Sense list
+senses_list = []
+for i, syn in enumerate(keyword_synsets):
+    senses_list.append(syn)
+    print("Sense " + str(i+1) + ' is ' + syn.definition())
+
+# • If the keyword has at least one sense as noun in WordNet, 
+# checks whether it appears as a noun in the Brown corpus. 
+# If the word is used as a noun, the program asks the users to enter a number which indicates which sense they want to select. 
+# Ensure you deal with unexpected and erroneous input.
+
+# checks whether it appears as a noun in the Brown corpus.
+from nltk.corpus import brown
+brown_keyword_tokens = [(word, pos) for word, pos in brown.tagged_words() if word == keyword and pos.startswith('NN')]
+# If the word is used as a noun
+sense_index = 0
+if len(brown_keyword_tokens):
+    # the program asks the users to enter a number which indicates which sense they want to select. 
+    sense_index = input("Select a sense: ")
+    # Ensure you deal with unexpected and erroneous input.
+    try:
+        sense_index = int(sense_index)
+        if sense_index < 1 or sense_index > len(senses_list):
+            print("Please input the valid sense number")
+    except:
+        print("Please input the correct sense number as a integer")
+else:
+    print("Keyword :", keyword, " doesn't appear as a noun in the Brown Corpus")
+
+# •	Displays the frequency of the keyword in the Brown corpus and of all the synonyms of the keyword for the selected sense. 
+# In all the cases, only the occurrences as nouns should be counted. 
+# For this step ignore the synonyms that are multi-word expressions.
+
+# get all synonyms of the keyword for selected sense 
+selected_sense = senses_list[sense_index - 1]
+for lemma in selected_sense.lemmas():
+    # only the occurrences as nouns should be counted
+    # For this step ignore the synonyms that are multi-word expressions.
+    if '_' not in lemma.name():
+        freqs = nltk.FreqDist(w.lower() for w, pos in brown.tagged_words() if w == lemma.name() and pos.startswith('NN'))
+        print(lemma.name(), ":", freqs.N())
+
+# •	Discuss why counting the frequency of the multi-word expressions 
+# which are synonyms with the keyword is difficult in Brown corpus using the code you wrote
+
+# why it is difficult
+
+# if we want to count the frequency of the multi-word expressions,
+# first, we should tokenize all the text of brown corpus
+# next, find the matched multi-words expressions according to words ordering
+# last, calculate the frequency of them
+
+# my solution 1
+
+# we can use n-grams method for multi-words expressions
+# if phrase has two words, we can use bi-gram, for three words, tri-gram, so on.
+
+# my solution 2
+# we can use MWETokenizer in nltk.tokenize package
+# A MWETokenizer takes a string which has already been divided into tokens and retokenizes it, merging multi-word expressions into single tokens, using a lexicon of MWEs
+
+
+
+
+
+
+
---
+++ sead_part2_task2.py
@@ -0,0 +1,81 @@
+# Part-2: (Python program)
+
+import nltk
+from nltk.corpus import wordnet as wn
+from nltk.corpus import brown
+# input keyword
+keyword = input("Enter a word: ")
+# input number
+number = input("Enter a number: ")
+# convert number string to integer number
+try:
+    number = int(number)
+    if number < 1:
+        number = 1
+except:
+    number = 1
+# get tagged words from brown corpus
+brown_tagged = brown.tagged_words()
+# get bigrams from brown corpus
+word_tag_pairs = list(nltk.bigrams(brown_tagged))
+
+# find words with specific POS tag before another specific POS tag
+def find_before_words(before_pos, after_pos, keyword,reverse=False):
+    before_list = None
+    # reverse means whether the word is before or after
+    # if reverse is False, find before word with before_pos POS tagg which appears before after_pos POS tag with keyword
+    # for instance, if you want adjective word before noun word 'shool', before_pos starts with 'JJ', after_pos starts with 'NN'
+    # and find prior word(in below code, a)
+    # if reverse is True, find next word (in below code, b)
+    if reverse:
+        before_list = [b[0] for (a, b) in word_tag_pairs if a[1].startswith(before_pos) and b[1].startswith(after_pos) and a[0]==keyword]
+    else:
+        before_list = [a[0] for (a, b) in word_tag_pairs if a[1].startswith(before_pos) and b[1].startswith(after_pos) and b[0]==keyword]
+    before_list = list(set(before_list))
+    return before_list
+
+# calculate frequence of founded word
+# this logic is like above
+def calc_bigram_frequency(before_pos, after_pos, keyword, before_list, reverse=False):
+    freq_list = []
+    for before in before_list:
+        dist = None
+        if reverse:
+            dist = nltk.FreqDist(b[0] for (a, b) in word_tag_pairs if a[1].startswith(before_pos) and b[1].startswith(after_pos) and a[0]==keyword and b[0] == before)
+        else:
+            dist = nltk.FreqDist(a[0] for (a, b) in word_tag_pairs if a[1].startswith(before_pos) and b[1].startswith(after_pos) and b[0]==keyword and a[0] == before)
+        freq_list.append((dist.N(), before))
+    return freq_list
+
+def find_most_before_words(before_pos, after_pos, keyword, number, reverse=False):
+    # find words with specific POS tag before another specific POS tag
+    words_list = find_before_words(before_pos, after_pos, keyword, reverse)
+    # calculate frequence of founded word
+    freq_list = calc_bigram_frequency(before_pos, after_pos, keyword, words_list, reverse)
+
+    # get valid length
+    max_len = min(len(freq_list), number)
+
+    # sort by frequency
+    freq_list = sorted(freq_list, key=lambda x: x[0], reverse=True)[0:max_len]
+    return freq_list
+
+# (b) & (d) finds which adjective appears the most often immediately before the given noun. Display the frequency of this adjective. 
+most_adjective_list = find_most_before_words('JJ', 'NN', keyword, number)
+print("Adjectives that appear before ", most_adjective_list)
+
+# (c) Using similar processing, take this adjective and find which noun appears most frequently after this adjective in the Brown corpus. 
+for (freq, adjective) in most_adjective_list:
+    most_noun_list = find_most_before_words('JJ', 'NN', adjective, 1, True)
+    if most_noun_list:
+        print('The most frequent noun that follows the adjective '+ adjective + ' is ' + most_noun_list[0][1] + ' frequency: ' + str(most_noun_list[0][0]))
+    
+
+
+
+
+
+
+
+
+
